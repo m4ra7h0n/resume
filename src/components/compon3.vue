f27234d3075ea2dfd1e3f82f83c838e533fcf8f9
x-j-j <2849771625@qq.com>
2022-10-17 17:28:19 +0800

    homework

---
+++ src/components/compon3.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="info">
+    <h1>作品集/PROFILES</h1>
+    <transition
+      :duration="{ enter: 2500, leave: 1500 }"
+      enter-active-class="animated fadeInRight">
+      <div class="cont"  v-if="show">
+        <el-row :gutter="20">
+          <template v-for="item in profiles">
+            <el-col :key="item.name" :span="12" :xs="24">
+              <div class="item">
+                <h2 v-text="item.name"></h2>
+                <p v-text="'技术栈: ' + item.skills"></p>
+                <p v-text="item.content"></p>
+                <p v-text="'github地址: ' + item.github"></p>
+              </div>
+            </el-col>
+          </template>
+        </el-row>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      show: false,
+      profiles: [
+        {
+          name: 'vue个人简历',
+          skills: 'vue vue-cli vue-router vuex element-ui',
+          content: '本项目',
+          github: 'https://github.com/x-j-j/resume'
+        },
+        {
+          name: 'learning-anything',
+          skills: 'maven SpringBoot SpringCloudAlibaba Dubbo Security Nacos Sentinel...',
+          content: '记录一切学习过的代码和源码',
+          github: 'https://github.com/x-j-j/learning-anything'
+        }
+      ]
+    }
+  },
+  methods: {
+    timeout () {
+      setTimeout(() => {
+        this.show = true
+      }, 1000)
+    }
+  },
+  mounted () {
+    this.timeout()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../less/index.less';
+.info {
+  .info;
+  .cont {
+    .cont;
+    width: 90%;
+    .item {
+      background-color: rgba(59, 123, 13, 0.2);
+      padding: 10px;
+      border-radius: 15px;
+      margin: 10px auto;
+      @media screen and (max-width: 768px) {
+        padding: 5px;
+        margin: 5px auto;
+      }
+      &:hover {
+        border-radius: 25px;
+        background-color: rgba(10, 135, 87, 0.3);
+      }
+      p {
+        text-align: left;
+        text-indent: 28px;
+        font-size: 1.1rem;
+        line-height: 1.2;
+        @media screen and (max-width: 768px) {
+          text-indent: 12px;
+          font-size: 0.7rem;
+          line-height: 1.1;
+        }
+      }
+    }
+  }
+}
+</style>
